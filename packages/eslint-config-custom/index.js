262463203d0be4a1e980218aacb4802fe05d7f7d
Egi Ginting <exneval.rayz@gmail.com>
2022-08-24 17:47:04 +0700

    chore(packages/eslint-config-custom): add react hook eslint
    Signed-off-by: Egi Ginting <exneval.rayz@gmail.com>

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["next", "turbo", "plugin:prettier/recommended"],
+  extends: ["next/core-web-vitals", "turbo", "plugin:prettier/recommended"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
